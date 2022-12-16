0522a7312f6e90262b4d6988d9b39dde240b70d3
adelmojsantos <adelmojsantos1985@gmail.com>
2022-12-16 13:25:07 -0300

    Corrigindo Transactions styles

--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -34,5 +34,4 @@ interface PriceHighlightProps {
 
 export const PriceHighligh = styled.span<PriceHighlightProps>`
   color: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme["red-300"]};
-  font-size: 1.6rem;
 `;
